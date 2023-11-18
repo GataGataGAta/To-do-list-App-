975e3a15dbeed8fe0c15a1dd4a63fd9f4c185d73
gata <hayato.yamagata@icloud.com>
2023-11-18 16:00:06 +0900

    go_routerを設定

---
+++ lib/router.dart
@@ -0,0 +1,24 @@
+import 'package:go_router/go_router.dart';
+import 'package:todoapp/importer.dart';
+import 'package:todoapp/view/list_page.dart';
+import 'package:todoapp/view/login_page.dart';
+
+/// The route configuration.
+final GoRouter MyRouter = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return loginPage();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'list',
+          builder: (BuildContext context, GoRouterState state) {
+            return const listPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todoapp/constant/colors.dart';
 
 class loginPage extends StatelessWidget {
@@ -115,7 +116,9 @@ class loginPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  context.go('/list');
+                },
                 child: const Text(
                   'Log in',
                   style: TextStyle(fontSize: 45),
